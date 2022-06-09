85d5c8883a21a76c38f4bb8d94996868f4e29af5
Teme Sahle <tsahle@terradatum.com>
2022-06-09 17:04:45 -0400

    experimenting with Router and Outlet to create persistent navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navbar!!</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+const Cart = () => {
+  return(
+    <div style={{ background: 'olive', color: 'lightpink', border: '2px dotted gray', margin: '15px', padding: '10px' }}>
+      <h1>Welcome to your shopping cart!</h1>
+    </div>
+  )
+}
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='cart' element={<Cart />} />
+      </Route>
+    </Routes>
+  )
 };
 
 export default App;
